89f419bebcba22f4e9a2918cecc03c200fcbc8d0
João Victor Ramalho Alves <joaovictorramalho7@gmail.com>
2021-10-19 11:24:39 -0300

    Create /signin/callback route

--- src/routes.ts
+++ src/routes.ts
@@ -6,4 +6,10 @@ const router = Router();
 
 router.post('/authenticate', new AuthenticateUserControlle().handle);
 
+router.get('/signin/callback', (request, response) => {
+  const { code } = request.query;
+
+  return response.json(code);
+});
+
 export { router };
