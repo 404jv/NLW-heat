293b4730111f5f03a49cd4fecc48aa274c354fa1
João Victor Ramalho Alves <joaovictorramalho7@gmail.com>
2021-10-19 23:27:47 -0300

    Create route github

--- src/routes.ts
+++ src/routes.ts
@@ -4,6 +4,12 @@ import { AuthenticateUserController } from './controllers/AuthenticateUserContro
 
 const router = Router();
 
+router.get('/github', (request, response) => {
+  return response.redirect(
+    `https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`
+  );
+});
+
 router.post('/authenticate', new AuthenticateUserController().handle);
 
 router.get('/signin/callback', (request, response) => {
