d94d285802cb3321c0fe2a730641c3dfe7fec150
João Victor Ramalho Alves <joaovictorramalho7@gmail.com>
2021-10-20 22:29:43 -0300

    Add socket to render new messages

--- web/src/components/MessageList/index.tsx
+++ web/src/components/MessageList/index.tsx
@@ -4,6 +4,7 @@ import styles from './styles.module.scss';
 
 import logoImage from '../../../assets/logo.svg';
 import { useEffect, useState } from 'react';
+import io from 'socket.io-client';
 
 interface Message {
   id: string;
@@ -15,8 +16,30 @@ interface Message {
   }
 }
 
+const messagesQueue: Message[] = [];
+
+const socket = io('http://localhost:3333');
+
+socket.on('new_message', (newMessage: Message) => {
+  messagesQueue.push(newMessage)
+});
+
 export function MessageList() {
   const [messages, setMessages] = useState<Message[]>([]);
+
+  useEffect(() => {
+    setInterval(() => {
+      if (messagesQueue.length > 0) {
+        setMessages(prevState => [
+          messagesQueue[0],
+          prevState[0],
+          prevState[1]
+        ].filter(Boolean));
+
+        messagesQueue.shift();
+      }
+    }, 3000);
+  }, []);
 
   useEffect(() => {
     api.get<Message[]>('messages/last3').then(response => {
