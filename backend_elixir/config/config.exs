66f9550f4cd4286f6ebd3c0e17b0800f6a29ab03
João Victor Ramalho Alves <joaovictorramalho7@gmail.com>
2021-10-30 14:35:13 -0300

    Init Elixir

---
+++ backend_elixir/config/config.exs
@@ -0,0 +1,31 @@
+# This file is responsible for configuring your application
+# and its dependencies with the aid of the Mix.Config module.
+#
+# This configuration file is loaded before any dependency and
+# is restricted to this project.
+
+# General application configuration
+use Mix.Config
+
+config :backend_elixir,
+  ecto_repos: [BackendElixir.Repo]
+
+# Configures the endpoint
+config :backend_elixir, BackendElixirWeb.Endpoint,
+  url: [host: "localhost"],
+  secret_key_base: "UDLAReQ1k5Pq3wYux94xfbiRMqz6k4pkoKza3ytpQo2TGiMY3g1DMFeg2uE2JZLx",
+  render_errors: [view: BackendElixirWeb.ErrorView, accepts: ~w(json), layout: false],
+  pubsub_server: BackendElixir.PubSub,
+  live_view: [signing_salt: "UYiUWVPb"]
+
+# Configures Elixir's Logger
+config :logger, :console,
+  format: "$time $metadata[$level] $message\n",
+  metadata: [:request_id]
+
+# Use Jason for JSON parsing in Phoenix
+config :phoenix, :json_library, Jason
+
+# Import environment specific config. This must remain at the bottom
+# of this file so it overrides the configuration defined above.
+import_config "#{Mix.env()}.exs"
---
+++ backend_elixir/lib/backend_elixir/application.ex
@@ -0,0 +1,34 @@
+defmodule BackendElixir.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  def start(_type, _args) do
+    children = [
+      # Start the Ecto repository
+      BackendElixir.Repo,
+      # Start the Telemetry supervisor
+      BackendElixirWeb.Telemetry,
+      # Start the PubSub system
+      {Phoenix.PubSub, name: BackendElixir.PubSub},
+      # Start the Endpoint (http/https)
+      BackendElixirWeb.Endpoint
+      # Start a worker by calling: BackendElixir.Worker.start_link(arg)
+      # {BackendElixir.Worker, arg}
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: BackendElixir.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+
+  # Tell Phoenix to update the endpoint configuration
+  # whenever the application is updated.
+  def config_change(changed, _new, removed) do
+    BackendElixirWeb.Endpoint.config_change(changed, removed)
+    :ok
+  end
+end
