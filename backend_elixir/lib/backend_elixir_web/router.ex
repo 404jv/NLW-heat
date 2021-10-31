2d3c8f3242c32f154e493a76c8a11fd76bfae420
João Victor Ramalho Alves <joaovictorramalho7@gmail.com>
2021-10-31 19:10:23 -0300

    Create post route to MessagesController

--- backend_elixir/lib/backend_elixir_web/router.ex
+++ backend_elixir/lib/backend_elixir_web/router.ex
@@ -7,6 +7,8 @@ defmodule BackendElixirWeb.Router do
 
   scope "/api", BackendElixirWeb do
     pipe_through :api
+
+    post "/message", MessagesController, :create
   end
 
   # Enables LiveDashboard only for development
